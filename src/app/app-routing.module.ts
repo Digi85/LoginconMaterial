50745f9838e7b196812f1e5f91ed48845aaab89a
Digi85 <73599904+Digi85@users.noreply.github.com>
2021-10-12 19:44:48 -0500

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
-import { NgModule } from '@angular/core';
+import { ContenidoComponent } from './contenido/contenido.component';
+import { Contenido2Component } from './contenido2/contenido2.component';
+import { NgModule} from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: 'contenido',component:ContenidoComponent},
+  {path: 'contenido2', component:Contenido2Component}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatInputModule} from '@angular/material/input';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import { ContenidoComponent } from './contenido/contenido.component';
+import { Contenido2Component } from './contenido2/contenido2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContenidoComponent,
+    Contenido2Component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatButtonModule,
+    MatCardModule,
+    MatInputModule,
+    MatToolbarModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
